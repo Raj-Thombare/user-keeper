05b7a9d669e5f01458d8dc63db8db0efb20c65eb
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-04-20 17:59:23 +0530

    feat: add new user through form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import NewUser from './components/NewUser/NewUser'
-import Users from './components/Users/Users'
+import NewUser from "./components/NewUser/NewUser";
+import Users from "./components/Users/Users";
+import {useState} from 'react';
 
-const userData = [{
-  id:1,
-  username:'Rakaa',
-  age:25
-},
-{
-  id:2,
-  username:'Raj',
-  age:20
-},
-{
-  id:3,
-  username:'Rohit',
-  age:34
-}];
+const dummy_users = [
+  {
+    id: 1,
+    username: "Rakaa",
+    age: 25,
+  },
+  {
+    id: 2,
+    username: "Raj",
+    age: 20,
+  },
+  {
+    id: 3,
+    username: "Rohit",
+    age: 34,
+  },
+];
 
 function App() {
+
+  const [userData, setUserData] = useState(dummy_users);
+  
+  const userDataHandler = (userData) => {
+    setUserData((prevUser) => [userData, ...prevUser])
+  }
+
   return (
     <div className="App">
-      <NewUser/>
-      <Users data={userData}/>
+      <NewUser onGetUserData={userDataHandler}/>
+      <Users data={userData} />
     </div>
   );
 }
--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -1,18 +1,41 @@
 import './NewUser.css'
 import Card from '../UI/Card'
+import {useState} from 'react'
 
 const NewUser = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const getUserNameHandler = (e) => {
+    setEnteredName(e.target.value)
+  }
+
+  const getUserAgeHandler = (e) => {
+    setEnteredAge(e.target.value)
+  }
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    const newUserData = {
+      id:Math.random().toString(),
+      username:enteredName,
+      age:enteredAge
+    }
+    props.onGetUserData(newUserData);
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <Card className="form_container">
           <div>
             <label>Username</label>
-            <input type="text" />
+            <input type="text" onChange={getUserNameHandler} />
           </div>
           <div>
             <label>Age (years)</label>
-            <input type="number" />
+            <input type="number" onChange={getUserAgeHandler}/>
           </div>
           <div>
             <button type="submit">Add User</button>
