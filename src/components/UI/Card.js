d1dbd84a73969a6c1beffb223fd1b593a65d43d0
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2022-04-22 23:23:35 +0530

    fix typo in card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,6 @@ import styles from './Card.module.css'
 
 const Card = (props) => {
     const classes = `${styles.card} ` + props.className;
-    console.log(classes)
     return (
         <div className={classes}>
             {props.children}
